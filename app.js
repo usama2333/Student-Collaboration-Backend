d5d6e080d543fec7e19d2339c1926ebdd8cfabc4
usama2333 <usama.wizz@gmail.com>
2025-03-03 17:38:23 +0500

    node server is running and db is connected

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log('Database connection failed', err));
+
+module.exports = app;
